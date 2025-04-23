aa08cb17530f25074daca68c198ee4908ecb5eb7
User <User@DESKTOP-ONQUMV3.lan>
2025-04-23 17:25:10 +0200

    part_1

---
+++ src/part_1/Part_1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package part_1;
+
+/**
+ *Student number: 10487456
+ * student name: Ndaedzo Given Tshiovhe
+ * Assignment: part 1
+ * @author RC_Student_lab
+ */
+public class Part_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Registration registration = new Registration();
+         Login login = new Login(registration);
+        login.setVisible(true);
+                
+    }
+    
+}
